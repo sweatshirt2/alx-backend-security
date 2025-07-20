ebfa2996654aa83dff4ff76f113f742f7cb97226
sweatshirt <sweatshirtblue2@gmail.com>
2025-07-20 22:17:03 +0300

    week11:  ip tracking

---
+++ ip_tracking/middleware.py
@@ -0,0 +1,22 @@
+from .models import RequestLog
+
+class IPLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Get client IP address
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip_address = x_forwarded_for.split(',')[0]
+        else:
+            ip_address = request.META.get('REMOTE_ADDR')
+
+        # Log the request
+        RequestLog.objects.create(
+            ip_address=ip_address,
+            path=request.path
+        )
+
+        response = self.get_response(request)
+        return response
